6cc5ad35b66b2e8ee6e5498eb0b755c323aa660f
kingsley achumie <kingsonly13c@gmail.com>
2024-09-08 16:52:31 +0100

    initer

---
+++ app/Jobs/FormData/FormDataCreated.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Jobs\FormData;
+
+use App\Services\FormService;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class FormDataCreated implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * The data for creating the unit.
+     *
+     * @var array
+     */
+    private array $data;
+
+    /**
+     * Create a new job instance.
+     *
+     * @param array $data
+     */
+    public function __construct(array $data)
+    {
+        $this->data = $data;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @param FormService $service
+     * @return void
+     */
+    public function handle(): void
+    {
+    }
+}
